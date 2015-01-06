af305dc94041ff6faf00a7b6ca177a7c2fd6b687
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2015-01-06 23:50:57 +0000

    Sets new folder structure
    This is in order to avoid having one repository for each platform and with a lot of duplicated and difficult to update code.
    
    With this setup, the common code, the src/ folder and resources (images and audio), are shared for every platform. On top of that there is still room for specifying platform specific settings and files. In order to keep the src/ code linked between platforms they are symlinked and this way a change in Eclipse in the android project for example is automatically reflected in Xcode for the iOS project, all of that keeping the version control changes centralized.
    
    There is also a linking.sh script for symlinking everything easier.

---
+++ PORTOphone_android/src/ofApp.cpp
@@ -0,0 +1,1 @@
+../../src/ofApp.cpp
---
+++ linking.sh
@@ -0,0 +1,34 @@
+# run this from the project root
+
+# ANDROID
+
+cd PORTOphone_android/src
+ln -sf ../../src/* .
+cd ..
+
+cd bin/data/
+
+cd images
+ln -sf ../../../../data/images/* .
+
+cd ..
+
+cd sounds
+ln -sf ../../../../data/sounds/* .
+
+
+# OSX
+
+# cd PORTOphone_osx/src
+# ln -sf ../../src/* .
+# cd ..
+
+# cd bin/data/
+
+# cd images
+# ln -sf ../../../../data/images/* .
+
+# cd ..
+
+# cd sounds
+# ln -sf ../../../../data/sounds/* .
