eaf3c5640ca677b7c665ebc440deadbe5179ffd7
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2015-12-12 18:10:34 +0000

    Compiles Android from Android Studio (#35)

--- PORTOphone_android/srcJava/cc/openframeworks/portophone/OFActivity.java
+++ PORTOphone_android/srcJava/cc/openframeworks/portophone/OFActivity.java
@@ -1,4 +1,4 @@
-package cc.openframeworks.portophone;
+package cc.openframeworks.PORTOphone;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -13,19 +13,17 @@ public class OFActivity extends cc.openframeworks.OFActivity{
 
 	@Override
     public void onCreate(Bundle savedInstanceState)
-    { 
+    {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         String packageName = getPackageName();
 
         ofApp = new OFAndroid(packageName,this);
     }
-	
+
 	@Override
 	public void onDetachedFromWindow() {
 	}
-	
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -37,7 +35,7 @@ public class OFActivity extends cc.openframeworks.OFActivity{
         super.onResume();
         ofApp.resume();
     }
-    
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 	if (OFAndroid.keyDown(keyCode, event)) {
@@ -46,7 +44,7 @@ public class OFActivity extends cc.openframeworks.OFActivity{
 	    return super.onKeyDown(keyCode, event);
 	}
     }
-    
+
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
 	if (OFAndroid.keyUp(keyCode, event)) {
@@ -58,9 +56,9 @@ public class OFActivity extends cc.openframeworks.OFActivity{
 
 
 	OFAndroid ofApp;
-    
-	
-	
+
+
+
     // Menus
     // http://developer.android.com/guide/topics/ui/menus.html
     @Override
@@ -68,19 +66,19 @@ public class OFActivity extends cc.openframeworks.OFActivity{
     	// Create settings menu options from here, one by one or infalting an xml
         return super.onCreateOptionsMenu(menu);
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	// This passes the menu option string to OF
     	// you can add additional behavior from java modifying this method
     	// but keep the call to OFAndroid so OF is notified of menu events
     	if(OFAndroid.menuItemSelected(item.getItemId())){
-    		
+
     		return true;
     	}
     	return super.onOptionsItemSelected(item);
     }
-    
+
 
     @Override
     public boolean onPrepareOptionsMenu (Menu menu){
@@ -88,12 +86,12 @@ public class OFActivity extends cc.openframeworks.OFActivity{
     	//  you can add or remove menu options from here
     	return  super.onPrepareOptionsMenu(menu);
     }
-    
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
             super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-        	getWindow().getDecorView().setSystemUiVisibility(
+            getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -101,7 +99,7 @@ public class OFActivity extends cc.openframeworks.OFActivity{
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
-	
+
 }
 
 
